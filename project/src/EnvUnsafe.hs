0f5ec26a818bcc056b69a7570f264e42a0877814
Jim Mortenson <jimrmortenson@gmail.com>
2019-04-25 11:22:06 -0400

    Add `EnvUnsafe` to Project Directory
    This change adds the `EnvUnsafe` module to the project directory. It is
    listed as a dependency in the LangWk10 module so at least for testing
    purposes, we need it locally.

---
+++ project/src/EnvUnsafe.hs
@@ -0,0 +1,48 @@
+module EnvUnsafe where
+
+import Control.Monad(ap)
+
+--This monad will form the plumbing for the evaluation function
+
+data Unsafe a = Error String | Ok a deriving (Show, Eq)
+
+data EnvUnsafe e a = EnvUnsafe (e -> Unsafe a )
+
+-- function that just runs the function contained in EnvUnsafe
+runEnvUnsafe ::  (EnvUnsafe e a) -> e -> Unsafe a
+runEnvUnsafe (EnvUnsafe eu) e = eu e
+
+-- a way to easily return an error (for instance in do notation)
+err :: String -> EnvUnsafe e a
+err s = EnvUnsafe $ \ _ -> Error s
+
+
+-- a way to easily get the entire environment (for instance in do notation)
+getEnv :: EnvUnsafe e e
+getEnv = EnvUnsafe $ \ env -> Ok env
+
+
+instance Functor (EnvUnsafe e) where
+  -- fmap :: (a -> b) -> EnvUnsafe a -> EnvUnsafe b
+  fmap f origEnv = EnvUnsafe $ (\ env ->
+    case (runEnvUnsafe origEnv env) of (Ok v) -> Ok $ f v
+                                       (Error msg) -> Error msg)
+  -- make sure your implementation follows the functor laws
+
+--ignore this for now
+instance Applicative (EnvUnsafe e) where
+  pure = return
+  (<*>) = ap
+
+instance Monad (EnvUnsafe e) where
+  --return :: a -> EnvUnsafe a
+  return val = EnvUnsafe $ \ env -> Ok val
+
+  --(>>=) :: EnvUnsafe a -> (a -> EnvUnsafe b) -> EnvUnsafe b
+  origEnv >>= f = EnvUnsafe $ (\ env ->
+    case (runEnvUnsafe origEnv env) of (Ok val) -> (runEnvUnsafe (f val)  env)
+                                       (Error msg) -> Error msg)
+
+  -- make sure your implementation follows the Monad laws
+
+-- technical note: this could be done more succinctly with monad transformers, but it is also good practice to do it by hand
