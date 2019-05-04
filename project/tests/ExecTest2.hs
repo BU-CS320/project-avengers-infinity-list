d61083ac3976e08ac1744a61f424b351ab09b40e
Jason Hong <jasonhong@dhcp-wifi-8021x-168-122-14-1.bu.edu>
2019-05-04 17:03:39 -0400

    ExecTest2 commits

--- project/tests/ExecTest2.hs
+++ project/tests/ExecTest2.hs
@@ -8,7 +8,7 @@ import Eval
 import Check
 
 tests = testGroup "ExecTest2: To be merged..."
-  [ 
+  [
     testCase "List Operations: " $
       do
         assertEqual "[12,true,2.2]" (Ret (Ls ([I 12, B True, F 2.2])) [] []) (exec "[12,true,2.2]")
@@ -21,6 +21,10 @@ tests = testGroup "ExecTest2: To be merged..."
           "[1,2]++2"
           (RuntimeError "TypeMismatch: second argument (2) must be a list" [] [])
           (exec "[1,2]++2")
+        assertEqual
+          "[1,2,4,x]"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "[1,2,4,x]")
         assertEqual
           "[1,2,,,,,]"
           (ParseError)
@@ -35,6 +39,10 @@ tests = testGroup "ExecTest2: To be merged..."
           "1.0+5"
           (RuntimeError "TypeMismatch: Cannot add float 1.0 and integer 5" [] [])
           (exec "1.0+5")
+        assertEqual
+          "x+5"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "x+5")
         assertEqual
           "1.0+'c'"
           (RuntimeError "'c' is not a number" [] [])
@@ -54,9 +62,12 @@ tests = testGroup "ExecTest2: To be merged..."
           (RuntimeError "true is not a number" [] [])
           (exec "2.2-true")
         assertEqual
-          "2.2--"
-          (ParseError)
-          (exec "2.2--"),
+          "x-45"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "x-45")
+        assertEqual "-2" (Ret (I (-2)) [] []) (exec "-2")
+        assertEqual "-45.92" (Ret (F (-45.92)) [] []) (exec "-45.92")
+        assertEqual "2.2--" (ParseError) (exec "2.2--"),
 
     testCase "Arithmetic: Multiplication " $
       do
@@ -66,6 +77,10 @@ tests = testGroup "ExecTest2: To be merged..."
           "3.2*3"
           (RuntimeError "TypeMismatch: Cannot multiply float 3.2 and integer 3" [] [])
           (exec "3.2*3")
+        assertEqual
+          "3.2*x"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "3.2*x")
         assertEqual
           "3.2*true"
           (RuntimeError "true is not a number" [] [])
@@ -95,6 +110,14 @@ tests = testGroup "ExecTest2: To be merged..."
           (RuntimeError "TypeMismatch: Can only use // with Integer types" [] [])
           (exec "4.5555//3.293")
         assertEqual
+          "10/y"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "y is not in scope"])
+          (exec "10/y")
+        assertEqual
+          "14.2//z"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "z is not in scope"])
+          (exec "14.2//z")
+        assertEqual
           "3/true"
           (RuntimeError "true is not a number" [] [])
           (exec "3/true")
@@ -105,5 +128,76 @@ tests = testGroup "ExecTest2: To be merged..."
         assertEqual "//3.4" (ParseError) (exec "//3.4")
         assertEqual "3.4//" (ParseError) (exec "3.4//")
         assertEqual "3/"    (ParseError) (exec "3/")
-        assertEqual "3//"   (ParseError) (exec "3//")
+        assertEqual "3//"   (ParseError) (exec "3//"),
+
+    testCase "Arithmetic: Exponentiation " $
+      do
+         assertEqual "3.2^4.5" (Ret (F 187.575) [] []) (exec "3.2^4.5")
+         assertEqual "3**4"    (Ret (I 81) [] []) (exec "3**4")
+         assertEqual
+           "3.2^4"
+           (RuntimeError "TypeMismatch: Can only use ^ with Float types" [] [])
+           (exec "3.2^4")
+         assertEqual
+           "4**5.2"
+           (RuntimeError "TypeMismatch: Can only use ** with Integer types" [] [])
+           (exec "4**5.2")
+         assertEqual
+           "true^3"
+           (RuntimeError "true is not a number" [] [])
+           (exec "true^3")
+         assertEqual
+           "4** 'c'"
+           (RuntimeError "'c' is not a number" [] [])
+           (exec "4**'c'")
+         assertEqual
+           "x^4"
+           (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+           (exec "x^4")
+         assertEqual
+           "3**y"
+           (RuntimeError "Variable not found!" [] [UndefinedVarUse "y is not in scope"])
+           (exec "3**y")
+         assertEqual "^4.5" (ParseError) (exec "^4.5")
+         assertEqual "4.5^" (ParseError) (exec "4.5^")
+         assertEqual "**3"  (ParseError) (exec "**3")
+         assertEqual "3**"  (ParseError) (exec "3**"),
+
+    testCase "Arithmetic: Modulus " $
+      do
+        assertEqual "5%3" (Ret (I 2) [] []) (exec "5%3")
+        assertEqual "5%0" (RuntimeError "Error: Mod-by-Zero is undefined" [] []) (exec "5%0")
+        assertEqual "true%3" (RuntimeError "true is not a number" [] []) (exec "true%3")
+        assertEqual
+          "x%3"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "x%3")
+        assertEqual
+          "4.5%3"
+          (RuntimeError "TypeMismatch: Can only use % with Integer types" [] [])
+          (exec "4.5%3"),
+
+    testCase "Heterogeneous Arithmetic " $
+      do
+        assertEqual "5+3-10*2" (Ret (I (-12)) [] []) (exec "5+3-10*2")
+        assertEqual "5-10//3" (Ret (I 2) [] []) (exec "5-10//3")
+        assertEqual "10**2+5//2" (Ret (I 102) [] []) (exec "10**2+5//2")
+        assertEqual "4.2^7.9/80.2" (Ret (F 1045.9115) [] []) (exec "4.2^7.9/80.2")
+        assertEqual "5+3--10*2" (Ret (I 28) [] []) (exec "5+3--10*2")
+        assertEqual "5%2+1*10"  (Ret (I 11) [] []) (exec "5%2+1*10")
+        assertEqual
+          "5+1-true*2"
+          (RuntimeError "true is not a number" [] [])
+          (exec "5+1-true*2")
+        assertEqual
+          "5-x//32"
+          (RuntimeError "Variable not found!" [] [UndefinedVarUse "x is not in scope"])
+          (exec "5-x//32")
+        assertEqual
+          "2**3^4"
+          (RuntimeError "TypeMismatch: Can only use ^ with Float types" [] [])
+          (exec "2**3^4")
+        assertEqual "(4)///5+1" (ParseError) (exec "(4)///5+1")
+        assertEqual "5+-" (ParseError) (exec "5+-")
+        assertEqual "%//4" (ParseError) (exec "%//4")
   ]
